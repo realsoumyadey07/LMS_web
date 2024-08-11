ec37b5aafa44adaf1d222657994ff4546cf17712
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-08-11 12:31:10 +0530

    onBoarding page done

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -6,98 +6,99 @@ import ThemeSwitcher from "../utils/ThemeSwitcher";
 import { HiOutlineMenuAlt3, HiOutlineUserCircle } from "react-icons/hi";
 
 type Props = {
-     open: boolean;
-     setOpen: (open: boolean) => void;
-     activeItem: number;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  activeItem: number;
 };
 
 const Header: FC<Props> = ({ activeItem, setOpen }) => {
-     const [active, setActive] = useState(false);
-     const [openSidebar, setOpenSidebar] = useState(false);
+  const [active, setActive] = useState(false);
+  const [openSidebar, setOpenSidebar] = useState(false);
 
-     const handleClose = (e: any) => {
-          if (e.target.id === "screen") {
-               setOpenSidebar(false);
-          }
-     };
+  const handleClose = (e: any) => {
+    if (e.target.id === "screen") {
+      setOpenSidebar(false);
+    }
+  };
 
-     useEffect(() => {
-          const handleScroll = () => {
-               if (window.scrollY > 80) {
-                    setActive(true);
-               } else {
-                    setActive(false);
-               }
-          };
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 80) {
+        setActive(true);
+      } else {
+        setActive(false);
+      }
+    };
 
-          window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-          return () => {
-               window.removeEventListener("scroll", handleScroll);
-          };
-     }, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-     return (
-          <div className="w-full relative">
-               <div
-                    className={`${active
-                              ? "dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
-                              : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"
-                         }`}
-               >
-                    <div className="w-[95%] 800px:w-[92%] m-auto py-2 h-full">
-                         <div className="w-full h-[80px] flex items-center justify-between p-3">
-                              <div>
-                                   <Link
-                                        href={"/"}
-                                        className={`text-[25px] font-Poppins font-[500] text-black dark:text-white`}
-                                   >
-                                        Youdemy
-                                   </Link>
-                              </div>
-                              <div className="flex items-center">
-                                   <NavItems activeItem={activeItem} isMobile={false} />
-                                   <ThemeSwitcher />
-                                   <HiOutlineUserCircle
-                                        size={25}
-                                        className="hidden 800px:block cursor-pointer dark:text-white text-black mr-3"
-                                        onClick={() => setOpen(true)}
-                                   />
-                                   <div className="800px:hidden mr-2">
-                                        <HiOutlineMenuAlt3
-                                             size={25}
-                                             className="cursor-pointer dark:text-white text-black"
-                                             onClick={() => setOpenSidebar(true)}
-                                        />
-                                   </div>
-                              </div>
-                         </div>
-                    </div>
-                    {/* mobile sidebar */}
-                    {openSidebar && (
-                         <div
-                              id="screen"
-                              className="800px:hidden fixed w-full h-screen top-0 left-0 z-[99999] dark:bg-[unset] bg-[#00000024]"
-                              onClick={handleClose}
-                         >
-                              <div className="w-[70%] fixed z-[999999999] h-screen bg-white dark:bg-slate-900 dark:bg-opacity-90 top-0 right-0">
-                                   <NavItems activeItem={activeItem} isMobile={true} />
-                                   <HiOutlineUserCircle
-                                        size={25}
-                                        className="cursor-pointer ml-5 my-2 text-black dark:text-white"
-                                        onClick={() => setOpen(true)}
-                                   />
-                                   <br />
-                                   <br />
-                                   <p className="text-[16px] px-2 pl-5 text-black dark:text-white">
-                                        Copyright @ 2024 Youdemy
-                                   </p>
-                              </div>
-                         </div>
-                    )}
-               </div>
+  return (
+    <div className="w-full relative">
+      <div
+        className={`${
+          active
+            ? "dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
+            : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"
+        }`}
+      >
+        <div className="w-[95%] 800px:w-[92%] m-auto py-2 h-full">
+          <div className="w-full h-[80px] flex items-center justify-between p-3">
+            <div>
+              <Link
+                href={"/"}
+                className={`text-[25px] font-Poppins font-[500] text-black dark:text-white`}
+              >
+                Youdemy
+              </Link>
+            </div>
+            <div className="flex items-center">
+              <NavItems activeItem={activeItem} isMobile={false} />
+              <ThemeSwitcher />
+              <HiOutlineUserCircle
+                size={25}
+                className="hidden 800px:block cursor-pointer dark:text-white text-black mr-3"
+                onClick={() => setOpen(true)}
+              />
+              <div className="800px:hidden mr-2">
+                <HiOutlineMenuAlt3
+                  size={25}
+                  className="cursor-pointer dark:text-white text-black"
+                  onClick={() => setOpenSidebar(true)}
+                />
+              </div>
+            </div>
           </div>
-     );
+        </div>
+        {/* mobile sidebar */}
+        {openSidebar && (
+          <div
+            id="screen"
+            className="800px:hidden fixed w-full h-screen top-0 left-0 z-[99999] dark:bg-[unset] bg-[#00000024]"
+            onClick={handleClose}
+          >
+            <div className="w-[70%] fixed z-[999999999] h-screen bg-white dark:bg-slate-900 dark:bg-opacity-90 top-0 right-0">
+              <NavItems activeItem={activeItem} isMobile={true} />
+              <HiOutlineUserCircle
+                size={25}
+                className="cursor-pointer ml-5 my-2 text-black dark:text-white"
+                onClick={() => setOpen(true)}
+              />
+              <br />
+              <br />
+              <p className="text-[16px] px-2 pl-5 text-black dark:text-white">
+                Copyright @ 2024 Youdemy
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- client/app/components/Route/Hero.tsx
+++ client/app/components/Route/Hero.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { FC } from 'react'
+import { BiSearch } from 'react-icons/bi';
 
-const Hero = () => {
+type Props = {};
+
+const Hero: FC<Props> = (props) => {
   return (
-    <div>
-      
+    <div className='w-full 1000px:flex items-center'>
+      <div className='absolute top-[130px] left-1/2 transform -translate-x-1/2 xs:left-[unset] xs:top-[unset] xs:right-0 xs:bottom-0 lg:h-[700px] lg:w-[700px] md:h-[600px] md:w-[600px] sm:h-[50vh] sm:w-[50vh] xs:h-[200px] xs:w-[200px] h-[150px] w-[150px] hero_animation rounded-full'></div>
+      <div className='1000px:w-[40%] flex 1000px:min-h-screen items-center justify-center pt-[70px] 1000px:pt-[0] z-10'>
+        <Image
+          src={require("../../../public/assests/hero-pic1.png")}
+          alt=''
+          className='object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-[auto] z-[10]'
+        />
+      </div>
+      <div className='1000px:w-[60%] flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[150px]'>
+        <h2 className='dark:text-white text-[#000000c7] text-[30px] px-3 w-full 1000px:text-[70px] font-600 font-Josefin py-2 1000px:leading-[75px] '>Improve your Online Learning Experience Better Instantly</h2>
+        <br />
+        <p className='dark:text-[#edfff4] text-[#000000ac] font-Josefin font-[600] text-[18px] 1500px:!w-[55%] 1100px:!w-[78%]'>We have 40k+ Online courses & 500k+ Online registered student. Find your desired Courses from here.</p>
+        <br />
+        <br />
+        <div className='1500px:w-[55%] 1100px:w-[78%] w-[90%] h-[50px] bg-transparent relative'>
+          <input 
+          type="search" 
+          placeholder='Search courses here...'
+          className='bg-transparent border dark:border-none dark:bg-[#575757] dark:placeholder:text-[#ffffffdd] rounded-[5px] p-2 w-full h-full outline-none text-[#0000004e] dark:text-[#ffffffe6] text-[20px] font-[500] font-Josefin'
+          />
+          <div className='absolute flex items-center justify-center w-[50px] cursor-pointer h-[50px] right-0 top-0 bg-[#2942ff] rounded-r-[5px]'>
+            <BiSearch className='text-white' size={30}/>
+          </div>
+        </div>
+        <br />
+        <br />
+        <div className='1500px:w-[55%] 1100px:w-[78%] w-[90%] flex items-center'>
+          <img src="https://edmy-react.hibootstrap.com/images/banner/client-3.jpg" alt="" className='rounded-full' />
+          <img src="https://edmy-react.hibootstrap.com/images/banner/client-1.jpg" alt="" className='rounded-full ml-[-20px]'/>
+          <img src="https://edmy-react.hibootstrap.com/images/banner/client-2.jpg" alt="" className='rounded-full ml-[-40]'/>
+          <p className='font-Josefin dark:text-[#edfff4] text-[#000000b3] 1000px:pl-3 text-[18px] font-[600]'>500k+ People already trusted us.{" "}
+            <Link 
+            href={"/courses"}
+            className='dark:text-[#46e256] text-[crimson]'
+            >
+              View Courses
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
